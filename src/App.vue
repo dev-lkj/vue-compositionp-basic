f5c767f2b8a41882959a3756d41373c8bedcf814
JIN-WIN\jin-win <2_kj@daum.net>
2024-06-11 22:27:45 +0900

    vue composition api practice

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,130 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <user-data  :first-name="firstName" :last-name="lastName" ></user-data>
+    <!-- <h2>{{ userName }}</h2>     -->
+    <!-- <h3>{{ age }}</h3> -->
+    <!-- <h2>{{ user.name }}</h2> -->
+    <!-- <h3>{{ user.age }}</h3> -->
+
+    <button @click="setAge">Change Age</button>
+    <div>
+      <input type ="text" placeholder="First Name" @input="setFirstName" v-model="firstName"/>
+      <input type ="text" placeholder="Last Name" v-model="lastName" ref="lastNameInput"/>
+      <button @click="setLastName">Set Last Name</button>
+    </div>
   </section>
 </template>
 
 <script>
-import { ref} from 'vue';
+import { ref, 
+  computed,
+  watch, provide,
+  // reactive, isReactive, isRef, toRefs 
+} from 'vue';
+import UserData from './components/UserData.vue';
 
 export default {
+  components: {
+    UserData,
+  },
   setup(){
-    const uName = ref('Maximilian');
+    // const uName = ref('Maximilian');
+    const uAge = ref(31);
+    // const user = reactive({
+    //   name : 'Maximilian',
+    //   age: 31,
+    // });
+    const firstName = ref('');
+    const lastName = ref('')
+    const lastNameInput = ref(null);
+
+    
+    provide('userAge', uAge);
+
+    const uName = computed(function(){
+      return firstName.value + ' ' + lastName.value;
+    });
+
+    
+    watch([uAge, uName], function(newValues, oldValues){
+      console.log('Old Age: '+ oldValues[0]);
+      console.log('New Age: '+ newValues[0]);
+      console.log('Old Name: '+ oldValues[1]);
+      console.log('New Name: '+ newValues[1]);
+    });
+
+    // uName.value = 'Max';
+    // console.log(uAge.value)
+    // console.log(user.name, user.age);
+    // console.log(isRef(uAge))
+    // console.log(isReactive(user), user.age);
+    
+
+    // setTimeout(function(){
+    //   // uName.value = 'Max';
+    //   // uAge.value = 32;
+    //   // user.value.name = 'Max',
+    //   // user.value.age = 32;
+    //   user.name = 'Max',
+    //   user.age = 32;
+    // },2000);
+
+    // const userRefs = toRefs(user);
+
+
+    function setNewAge(){
+      // user.age = 33;
+      uAge.value = 33;
+    }
+
+    // function setFirstName(event){
+    //   firstName.value = event.target.value;
+    // }
+
+    // function setLastName(event){
+    function setLastName(){
+      // lastName.value = event.target.value;
+      // lastName.value = this.$refs.lastNameInput.value;
+      lastName.value = this.lastNameInput.value.value;
+    }
+
+
 
     return {
-      userName : uName
-      
-      
+      userName: uName,
+      age : uAge,
+      // userName : user.value.name,
+      // age: user.value.age,
+      // user : user,
+      // userName : userRefs.name, age: userRefs.age,
+      setAge : setNewAge,
+      // setFirstName : setFirstName,
+      setLastName : setLastName,
+      firstName,
+      lastName,
+      lastNameInput,
     };
   },
   // data() {
   //   return {
   //     userName: 'Maximilian',
+  // age: 31,
   //   };
   // },
-  
+  // methods: {
+  //   setNewAge() {
+  //     this.age = 32;
+  //   },
+  // },
+  // computed: {},
+  // watch: {
+  //   age(val) {
+  //     console.log(val);
+  //   },
+  // },
+  // provide(){
+  //   return {age: this.age};
+  // }
 };
 </script>
 
---
+++ src/components/UserData.vue
@@ -0,0 +1,65 @@
+<template>
+    <h2>{{ userName }}</h2>
+    <h3> {{ age }}</h3>
+    <slot></slot>
+</template>
+
+<script>
+import { computed, inject, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue';
+
+export default{
+    // inject: {},
+    // props: ['userName', 'age'],
+    props: ['firstName','lastName'],
+    // computed:{
+    //     userName() {
+    //         return this.firstName + ' ' + this.lastName;
+    //     },
+    // },
+    setup(props, context) {
+        const uName  = computed(function(){
+            return props.firstName + ' ' + props.lastName;
+        });
+
+        const age = inject('userAge');
+        // age.value = 32;
+
+        console.log(context);
+
+        onBeforeMount(function(){
+            console.log('onBeforeMount')
+        });
+
+        onMounted(function(){
+            console.log('onMounted')
+        });
+
+        onBeforeUpdate(function(){
+            console.log('onBeforeUpdate')
+        });
+        
+        onUpdated(function(){
+            console.log('onUpdated')
+        });
+
+        onBeforeUnmount(function(){
+            console.log('onBeforeUnmount')
+        });
+
+        onUnmounted(function(){
+            console.log('onUnmounted')
+        });
+        
+
+        // context.emit('save-data', 1); // this.$emit('save-data', 1);
+
+        return {
+            userName : uName, 
+            age,
+
+
+        };
+    },
+
+}
+</script>
